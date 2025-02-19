db9c4014115285816c00ff40423e348acade9bb3
Caveman-FatalGames <nexus.fatalgames@gmail.com>
2025-02-19 14:00:24 -0600

    Initial Build Format

---
+++ src/main/java/com/fatalgames/nexus/block/custom/TerrestrialTransmutationTable.java
@@ -0,0 +1,31 @@
+package com.fatalgames.nexus.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class TerrestrialTransmutationTable extends Block {
+    public TerrestrialTransmutationTable(Properties properties){
+        super(properties);
+    }
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        if(pEntity instanceof ItemEntity itemEntity) {
+            if(isValidItem(itemEntity.getItem())) {
+                itemEntity.setItem(new ItemStack(Items.COAL, itemEntity.getItem().getCount()));
+            }
+        }
+
+        super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(Items.SAND);
+    }
+
+}
